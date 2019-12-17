11573f35f5c8e07aa8dfc4d3f057a6b6256cc034
Alex Zoric <alex.zoric@diomac.com>
2019-12-17 11:42:40 +0000

    Added constant to hold colors used in app.  Applying title to Categories screen and styling.

--- rn-meals-app/screens/CategoriesScreen.js
+++ rn-meals-app/screens/CategoriesScreen.js
@@ -1,34 +1,50 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, FlatList} from 'react-native';
+import { View, Text, StyleSheet, Button, FlatList, Touchable, TouchableOpacity, Platform } from 'react-native';
 import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors'
 
-const renderGridItem = (itemData) => {
-return <View style={styles.gridItem}>
-    <Text>{itemData.item.title} </Text>
-</View>
-};
 const CategoriesScreen = props => {
-    return(
+    const renderGridItem = (itemData) => {
+        return (
+            <TouchableOpacity style={styles.gridItem} onPress={() => {
+                props.navigation.navigate({routeName: 'CategoryMeals'})
+            }}>
+                <View>
+                    <Text>{itemData.item.title} </Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
+    return (
         <FlatList
-         data={CATEGORIES} 
-         renderItem={renderGridItem} 
-         numColumns={2} 
-         />
+            data={CATEGORIES}
+            renderItem={renderGridItem}
+            numColumns={2}
+        />
     );
 };
 
-const styles = StyleSheet.create({
-screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-},
+CategoriesScreen.navigationOptions = {
+ headerTitle: 'Meals Categories',
+ headerStyle: {
+     backgroundColor: Platform.OS==='android' ? Colors.primaryColor : ''
+ },
+ headerTintColor: Platform.OS==='android' ? 'white' : Colors.primaryColor
+};
 
-gridItem: {
-    flex: 1,
-    margin: 15,
-    height: 150
-}
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150
+    }
 });
 
 export default CategoriesScreen;
