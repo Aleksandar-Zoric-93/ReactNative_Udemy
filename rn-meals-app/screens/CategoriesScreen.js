de7d64a8e7c4f27c1af21728efb32fecb4c25923
Alex Zoric <alex.zoric@diomac.com>
2019-12-17 11:26:32 +0000

    Added dummy data file with different meal categories.  Added a category class with default constructor. Added grid to Categories Screen using Flatlist and grid styling.

--- rn-meals-app/screens/CategoriesScreen.js
+++ rn-meals-app/screens/CategoriesScreen.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button} from 'react-native';
+import { View, Text, StyleSheet, Button, FlatList} from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
 
+const renderGridItem = (itemData) => {
+return <View style={styles.gridItem}>
+    <Text>{itemData.item.title} </Text>
+</View>
+};
 const CategoriesScreen = props => {
     return(
-        <View style={styles.screen}>
-            <Text>The Categories Screen</Text>
-            <Button title="Go to Meals" onPress={() => {
-                {/* Use replace instead of navigate when you don't want the user to be able to go back e.g. once they login */}
-                props.navigation.navigate({routeName: 'CategoryMeals'});
-            }} />
-        </View>
+        <FlatList
+         data={CATEGORIES} 
+         renderItem={renderGridItem} 
+         numColumns={2} 
+         />
     );
 };
 
@@ -18,6 +22,12 @@ screen: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+},
+
+gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
 }
 });
 
